b93d3cebeaaa0eec6221e8f1f751c4300cb10c99
Emanuel Stefan <es@es-dev.de>
2016-05-29 20:17:55 +0200

    first commit

---
+++ js/spring-browsersync.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	// check
+	var socket = new SockJS('/wsdevtools');
+	var stompClient = Stomp.over(socket);
+	stompClient.debug = null;
+	stompClient.connect({}, function(frame) {
+		console.log('Connected: ' + frame);
+		stompClient.subscribe('/wsdevtools/filesync', function(event) {
+				console.log(event.body);
+		});
+	});
+});
